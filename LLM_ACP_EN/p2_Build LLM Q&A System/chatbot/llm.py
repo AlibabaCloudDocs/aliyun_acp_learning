53670e4768de5cf6d534aebba3e59a851a4cb5df
haoznic <haoznic@gmail.com>
2026-01-26 18:21:35 +0800

    upd deps

--- LLM_ACP_EN/p2_Build LLM Q&A System/chatbot/llm.py
+++ LLM_ACP_EN/p2_Build LLM Q&A System/chatbot/llm.py
@@ -1,21 +1,25 @@
 import os
 from openai import OpenAI
-client = OpenAI(
-    api_key=os.getenv("DASHSCOPE_API_KEY"), # How to obtain the API Key: https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
-    base_url=os.getenv("DASHSCOPE_API_BASE")
-)
 
 def invoke(user_message, model_name="qwen-plus"):
+    client = OpenAI(
+        api_key=os.getenv("DASHSCOPE_API_KEY"), # How to obtain the API Key: https://www.alibabacloud.com/help/en/model-studio/get-api-key
+        base_url=os.getenv("DASHSCOPE_API_BASE")
+    )
     completion = client.chat.completions.create(
-        model=model_name, # Model list: https://help.aliyun.com/zh/model-studio/getting-started/models
+        model=model_name, # Model list: https://www.alibabacloud.com/help/en/model-studio/models
         messages=[{"role": "user", "content": user_message}]
     )
     # In production environments, it is recommended to handle invocation exceptions
     return completion.choices[0].message.content
 
 def invoke_with_stream_log(user_message, model_name="qwen-plus"):
+    client = OpenAI(
+        api_key=os.getenv("DASHSCOPE_API_KEY"), # How to obtain the API Key: https://www.alibabacloud.com/help/en/model-studio/get-api-key
+        base_url=os.getenv("DASHSCOPE_API_BASE")
+    )
     completion = client.chat.completions.create(
-        model=model_name, # Model list: https://help.aliyun.com/zh/model-studio/getting-started/models
+        model=model_name, # Model list: https://www.alibabacloud.com/help/en/model-studio/models
         messages=[{"role": "user", "content": user_message}],
         stream=True
     )
